3f2f967a7a1ca297bff2947f0f3f7dafe63a4ea3
lastOfNoldor <martynovoo95@gmail.com>
2025-06-26 00:02:15 +0300

    feat: ФЗ полностью + тесты
    Забыл прокоммитить разные стадии до завершения задачи

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
 public class Film {
+
+    private Long id;
+    @NotBlank
+    @NotNull
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Long duration;
 }
